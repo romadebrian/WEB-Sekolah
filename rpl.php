e408e4835f49ffc8dd3f4a575e11cc48b3ec9fcd
romadebrian <roma_coll04@yahoo.com>
2018-07-16 15:33:11 +0700

    PRL 2

--- rpl.php
+++ rpl.php
@@ -6,7 +6,7 @@
 <!-- Meta tag Keywords -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Sekolah CMS" />
+<meta name="keywords" content="Sekolah CMS, Source Code" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!--// Meta tag Keywords -->
