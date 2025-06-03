4cf04f35bc0346eff2e44f7296e9f1928d2cb556
romadebrian <roma_coll04@yahoo.com>
2025-06-04 06:17:05 +0700

    fix some error when add account when already exists

--- Admin/Proses_Input_Akun.php
+++ Admin/Proses_Input_Akun.php
@@ -5,18 +5,14 @@ $Username = $_POST['Username'];
 $Password = md5($_POST['Password']);
 $Level = $_POST['Level'];
 
-$sql = mysqli_query($koneksi,"SELECT * FROM `akun` WHERE username = '$Username' ") or die(mysql_error());
-if(mysqli_num_rows($sql) == 0)
-{
-	$input_akun="INSERT INTO `akun` (`username`, `password`, `level`) VALUES ('$Username', '$Password', '$Level');";
+$sql = mysqli_query($koneksi, "SELECT * FROM `akun` WHERE username = '$Username' ") or die(mysqli_error($koneksi));
+if (mysqli_num_rows($sql) == 0) {
+	$input_akun = "INSERT INTO `akun` (`username`, `password`, `level`) VALUES ('$Username', '$Password', '$Level');";
 	(mysqli_query($koneksi, $input_akun));
 	header('location:akun.php');
-}
-else
-{
+} else {
 	echo (" <SCRIPT LANGUAGE='JavaScript'>
 				window.alert('Username sudah Terdaftar')
 				window.location.href='akun.php';
 			</SCRIPT>");
 }
-?>
